c44ab21c45d4d52918c805ada529f68192660478
LuanVendt <luanvendt@gmail.com>
2023-10-17 16:08:26 -0300

    Caso de uso: Deletar pergunta

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -5,6 +5,16 @@ import { Question } from "@/domain/forum/enterprise/entities/question";
 export class InMemoryQuestionsRepository implements QuestionsRepository {
     public items: Question[] = []
 
+    async findById(id: string) {
+        const question = await this.items.find(item => item.id.toString() === id)
+
+        if (!question) {
+            return null
+        }
+
+        return question
+    }
+
     async findBySlug(slug: string) {
         const question = await this.items.find(item => item.slug.value === slug)
 
@@ -18,5 +28,11 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
     async create(question: Question) {
         this.items.push(question)
+    }
+
+    async delete(question: Question) {
+        const itemIndex = this.items.findIndex(item => item.id === question.id)
+
+        this.items.splice(itemIndex, 1)
     }
 }
