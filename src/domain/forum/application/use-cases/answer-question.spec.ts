f137b2bf8e9943d67041cb9c9570039195788002
LuanVendt <luanvendt@gmail.com>
2023-10-17 14:30:12 -0300

    Refatorando as pastas

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,9 +1,10 @@
-import { Answer } from '../entities/answer'
+
+import { Answer } from '../../enterprise/entities/answer'
 import { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
 const fakeAnswersRepository: AnswersRepository = {
-  create: async (answer: Answer) => {},
+  create: async (answer: Answer) => { },
 }
 
 test('create an answer', async () => {
