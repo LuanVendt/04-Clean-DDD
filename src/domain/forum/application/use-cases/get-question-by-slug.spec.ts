f84df2676dad59a0e57d2b914ef6f9db1bd7d358
LuanVendt <luanvendt@gmail.com>
2023-10-17 15:42:59 -0300

    Factories de testes

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,8 +1,7 @@
 
 
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeQuestion } from 'test/factories/make-question'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
-import { Question } from '../../enterprise/entities/question'
 import { Slug } from '../../enterprise/entities/value-objects/slug'
 import { GetQuestionBySlugUseCase } from './get-question-by-slug'
 
@@ -17,11 +16,8 @@ describe('Get Question By Slug', () => {
     })
 
     it('should be able to get a question by slug', async () => {
-        const newQuestion = Question.create({
-            authorId: new UniqueEntityID(),
-            title: 'Example-question',
-            slug: Slug.create('example-question'),
-            content: 'Example content',
+        const newQuestion = makeQuestion({
+            slug: Slug.create('example-question')
         })
 
         inMemoryQuestionsRepository.create(newQuestion)
